f89b76c3cffce6f6a015be904a5119bdcdddaed5
Charlie Dean <dean.charlie07@gmail.com>
2015-07-06 09:54:40 +0100

    Added install and update files, Updated readme

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod +x QuickMetasploit.py
+ln -s QuickMetasploit.py /usr/bin/quickmetasploit
+echo "Quick Metasploit Has Been Installed, Run quickmetasploit From A Shell To Start"
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git pull
+echo "Update Finished"
